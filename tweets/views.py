5503f3c120df37834f4592b509910a730cd4fff4
t3tsuvastr <doshankadyr2007@gmail.com>
2025-01-14 21:07:54 +0500

    working on tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,26 +1,62 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render , get_object_or_404 , redirect
 from .models import Tweet
-from .forms import TweetForm
+from .forms import TweetForm , CommentForm
 
 def tweet_list(request):
     tweets = Tweet.objects.all().order_by('-created_at')
-    return render(request, 'tweets/tweet_list.html' , {'tweets':tweets})
-
-def tweet_detail(request, slug):
-    tweet = get_object_or_404(Tweet, slug=slug)
-    return render(request, 'tweets/tweet_detail.html', {'tweet':tweet})
-
-@login_required
-def create_tweet(request):
     if request.method == 'POST':
         form = TweetForm(request.POST, request.FILES)
         if form.is_valid():
             tweet = form.save(commit=False)
             tweet.user = request.user
             tweet.save()
-            form.save_m2m() 
-            return redirect('home')
+            return redirect('tweet_list')
     else:
         form = TweetForm()
-    return render(request, 'tweets/create_tweet.html', {'form': form})
+    return render(request, 'tweets/tweet_list.html', {'tweets': tweets , 'form': form})
+
+
+@login_required
+def edit_tweet(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    if request.user != tweet.user:
+        return redirect('tweet_list')
+    if request.method == 'POST':
+        form = TweetForm(request.POST, request.FILES, instance=tweet)
+        if form.is_valid():
+            form.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweets/edit_tweet.html' , {'form': form})
+
+@login_required
+def delete_tweet(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    if request.user == tweet.user:
+        tweet.delete()
+    return redirect('tweet_list')
+
+@login_required
+def like_tweet(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    if request.user in tweet.likes.all():
+        tweet.likes.remove(request.user)
+    else:
+        tweet.likes.add(request.user)
+    return redirect('tweet_list')
+
+@login_required 
+def add_comment(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.tweet = tweet 
+            comment.save()
+            return redirect('tweet_list')
+    return redirect('tweet_list')
+
